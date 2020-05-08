d13c521e097b24380e9e159108ed76a1d461e048
VipinKT <ktvipin27@gmail.com>
2020-05-08 15:16:44 +0530

    initial commit

---
+++ app/src/main/java/com/ktvipin27/roomexplorer/sample/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.ktvipin27.roomexplorer.sample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Created by Vipin KT on 08/05/20
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
