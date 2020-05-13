7f6bcd0a5611252f64d65f5437860f4eda8ef674
VipinKT <ktvipin27@gmail.com>
2020-05-13 15:26:27 +0530

    minor fixes

--- library/src/main/java/com/ktvipin27/roomexplorer/ExplorerActivity.kt
+++ library/src/main/java/com/ktvipin27/roomexplorer/ExplorerActivity.kt
@@ -26,49 +26,45 @@ class ExplorerActivity : AppCompatActivity() {
 
     private lateinit var databaseClass: Class<out RoomDatabase>
     private lateinit var databaseName: String
-    private val tableNames = arrayListOf<String>()
     private val tableNamesAdapter: ArrayAdapter<String> by lazy {
         ArrayAdapter(
             this,
             android.R.layout.simple_spinner_item,
-            tableNames
+            arrayListOf<String>()
         ).apply { setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item) }
     }
-    private var selectedTableName: String = ""
+    private val tableNameSelectedListener = object : AdapterView.OnItemSelectedListener {
+        override fun onNothingSelected(parent: AdapterView<*>?) {}
+        override fun onItemSelected(
+            parent: AdapterView<*>?,
+            view: View?,
+            position: Int,
+            id: Long
+        ) = displayData()
+    }
+    private val selectedTableName
+        get() = sp_table.selectedItem as String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_explorer)
         setSupportActionBar(toolbar)
+
         sp_table.adapter = tableNamesAdapter
-        sp_table.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        sp_table.onItemSelectedListener = tableNameSelectedListener
 
-            override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
-            ) {
-                selectedTableName = tableNames[position]
-                displayCount(selectedTableName)
-                displayData(selectedTableName)
-            }
-
-        }
         intent.extras?.let {
             if (it.containsKey(RoomExplorer.KEY_DATABASE_CLASS)) {
                 databaseClass = it.get(RoomExplorer.KEY_DATABASE_CLASS) as Class<out RoomDatabase>
             } else {
-                throw java.lang.RuntimeException("No database class passed in the launch Intent.")
+                throw RuntimeException("No database class passed in the launch Intent.")
             }
             if (it.containsKey(RoomExplorer.KEY_DATABASE_NAME)) {
                 databaseName = it.getString(RoomExplorer.KEY_DATABASE_NAME, "")
             } else {
-                throw java.lang.RuntimeException("No database name passed in the launch Intent.")
+                throw RuntimeException("No database name passed in the launch Intent.")
             }
-        } ?: throw java.lang.RuntimeException("No data passed in the launch Intent.")
-
+        } ?: throw RuntimeException("No data passed in the launch Intent.")
 
         getTableNames()
     }
@@ -79,29 +75,21 @@ class ExplorerActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
-        R.id.action_add -> {
-            true
-        }
-        R.id.action_delete -> {
-            deleteTable(selectedTableName)
-            true
-        }
-        R.id.action_drop -> {
-            dropTable(selectedTableName)
-            true
-        }
+        R.id.action_add -> true
+        R.id.action_delete -> true.also { deleteTable() }
+        R.id.action_drop -> true.also { dropTable() }
         else -> super.onOptionsItemSelected(item)
     }
 
     private fun getTableNames() {
-        tableNames.clear()
+        tableNamesAdapter.clear()
         when (
             val queryResult = getData(Queries.GET_TABLE_NAMES)) {
             is QueryResult.Success -> {
                 val cursor = queryResult.data
                 cursor.moveToFirst()
                 do {
-                    tableNames.add(cursor.getString(0))
+                    tableNamesAdapter.add(cursor.getString(0))
                 } while (cursor.moveToNext())
                 cursor.close()
             }
@@ -110,82 +98,16 @@ class ExplorerActivity : AppCompatActivity() {
             }
         }
         tableNamesAdapter.notifyDataSetChanged()
-        if (tableNames.isNotEmpty())
+        if (!tableNamesAdapter.isEmpty)
             sp_table.setSelection(0)
     }
 
-    private fun deleteTable(tableName: String) {
-        showAlert(
-            getString(R.string.title_delete_table),
-            getString(R.string.message_delete_table, tableName),
-            getString(android.R.string.ok)
-        ) {
-            when (val queryResult = execute(Queries.DELETE_TABLE + tableName)) {
-                is QueryResult.Success -> {
-                    toast(R.string.message_operation_success)
-                    sp_table.setSelection(0)
-                }
-                is QueryResult.Error -> {
-                    toast(
-                        getString(
-                            R.string.message_operation_failed,
-                            queryResult.exception.message
-                        )
-                    )
-                }
-            }
-        }
-    }
-
-    private fun dropTable(tableName: String) {
-        showAlert(
-            getString(R.string.title_drop_table),
-            getString(R.string.message_drop_table, tableName),
-            getString(android.R.string.ok)
-        ) {
-            when (val queryResult = execute(Queries.DROP_TABLE + tableName)) {
-                is QueryResult.Success -> {
-                    toast(R.string.message_operation_success)
-                    if (tableNames.size < 1)
-                        refreshActivity()
-                    else
-                        getTableNames()
-                }
-                is QueryResult.Error -> {
-                    toast(
-                        getString(
-                            R.string.message_operation_failed,
-                            queryResult.exception.message
-                        )
-                    )
-                }
-            }
-        }
-    }
-
-    private fun displayCount(tableName: String) {
-        when (val queryResult = getData(Queries.GET_COUNT + tableName)) {
-            is QueryResult.Success -> {
-                val cursor = queryResult.data
-                cursor.moveToFirst()
-                val count = cursor.getInt(0)
-                tv_record_count.text = getString(R.string.number_of_records, count)
-                cursor.close()
-            }
-            is QueryResult.Error -> {
-                tv_record_count.text = getString(R.string.number_of_records, 0)
-                toast(getString(R.string.message_operation_failed, queryResult.exception.message))
-            }
-        }
-    }
-
-    private fun displayData(tableName: String) {
+    private fun displayData() {
         tl.removeAllViews()
-        when (val queryResult = getData(Queries.GET_TABLE_DATA + tableName)) {
+        when (val queryResult = getData(Queries.GET_TABLE_DATA + selectedTableName)) {
             is QueryResult.Success -> {
                 val cursor = queryResult.data
-
-                //display the first row of the table with column names of the table selected by the user
+                tv_record_count.text = getString(R.string.number_of_records, cursor.count)
                 val th = TableRow(this)
                 cursor.moveToFirst()
                 for (i in 0 until cursor.columnCount) {
@@ -225,6 +147,55 @@ class ExplorerActivity : AppCompatActivity() {
             }
             is QueryResult.Error -> {
                 //TODO handle error
+            }
+        }
+    }
+
+    private fun deleteTable() {
+        showAlert(
+            getString(R.string.title_delete_table),
+            getString(R.string.message_delete_table, selectedTableName),
+            getString(android.R.string.ok)
+        ) {
+            when (val queryResult = execute(Queries.DELETE_TABLE + selectedTableName)) {
+                is QueryResult.Success -> {
+                    toast(R.string.message_operation_success)
+                    displayData()
+                }
+                is QueryResult.Error -> {
+                    toast(
+                        getString(
+                            R.string.message_operation_failed,
+                            queryResult.exception.message
+                        )
+                    )
+                }
+            }
+        }
+    }
+
+    private fun dropTable() {
+        showAlert(
+            getString(R.string.title_drop_table),
+            getString(R.string.message_drop_table, selectedTableName),
+            getString(android.R.string.ok)
+        ) {
+            when (val queryResult = execute(Queries.DROP_TABLE + selectedTableName)) {
+                is QueryResult.Success -> {
+                    toast(R.string.message_operation_success)
+                    if (tableNamesAdapter.count < 2)
+                        refreshActivity()
+                    else
+                        getTableNames()
+                }
+                is QueryResult.Error -> {
+                    toast(
+                        getString(
+                            R.string.message_operation_failed,
+                            queryResult.exception.message
+                        )
+                    )
+                }
             }
         }
     }
